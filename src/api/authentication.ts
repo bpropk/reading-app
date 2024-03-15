395971e76553bf9d65f88f87a19defaac79dee29
Binh Tran <BBinhtony@gmail.com>
2024-03-15 15:29:02 +0700

    update API

--- src/api/authentication.ts
+++ src/api/authentication.ts
@@ -14,10 +14,27 @@ interface registerDto {
   password: string;
 }
 
+interface forgotPasswordDto {
+  email: string;
+}
+
+interface changePasswordDto {
+  token: string;
+  newPassword: string;
+}
+
 export const login = async (body: loginDto) => {
   return axios.post(`${process.env.API_URL}/auth/login`, body);
 };
 
 export const register = async (body: registerDto) => {
   return axios.post(`${process.env.API_URL}/auth/register`, body);
+};
+
+export const forgotPassword = async (body: forgotPasswordDto) => {
+  return axios.post(`${process.env.API_URL}/auth/forgot-password`, body);
+};
+
+export const changePassword = async (body: changePasswordDto) => {
+  return axios.post(`${process.env.API_URL}/auth/change-password`, body);
 };
