80b28edee1dd508fa13f9706bf810542890bb933
Binh Tran <BBinhtony@gmail.com>
2024-03-15 15:25:29 +0700

    update api register

--- src/api/authentication.ts
+++ src/api/authentication.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
 
 interface loginDto {
-  username?: string;
-  password?: string;
+  username: string;
+  password: string;
+}
+
+interface registerDto {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  address: string;
+  username: string;
+  password: string;
 }
 
 export const login = async (body: loginDto) => {
   return axios.post(`${process.env.API_URL}/auth/login`, body);
+};
+
+export const register = async (body: registerDto) => {
+  return axios.post(`${process.env.API_URL}/auth/register`, body);
 };
